7beb0588c300962d6acff79db051274522bd26e7
c0600299 <c0600299@SB-N208-12.lambton.on.ca>
2017-03-22 11:25:14 -0400

    Some building on Message, MessageController and MessageService

--- src/java/Messages/MessageController.java
+++ src/java/Messages/MessageController.java
@@ -5,10 +5,56 @@
  */
 package Messages;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.websocket.Session;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
 /**
  *
  * @author c0600299
  */
 public class MessageController {
     
+    private List<Session> people = new ArrayList<>();
+    private List<JsonObject> messages = new ArrayList<>();
+
+    public MessageController() {
+    }
+    
+    public List<Session> getPeople() {
+        return people;
+    }
+
+    public void setPeople(List<Session> people) {
+        this.people = people;
+    }
+
+    public List<JsonObject> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<JsonObject> messages) {
+        this.messages = messages;
+    }
+
+    Iterable<Session> getSessions() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addMessage(JsonObject json) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addSession(Session session) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean containsSession(Session session) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- src/java/Messages/MessageService.java
+++ src/java/Messages/MessageService.java
@@ -5,10 +5,56 @@
  */
 package Messages;
 
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+
 /**
  *
  * @author c0600299
  */
 public class MessageService {
-    
+
+    @GET
+    @Produces("application/json")
+    public JsonArray getAll() {
+        JsonArrayBuilder json = Json.createArrayBuilder();
+        for (String m : MessageController.getMessages()) {
+            json.add(m);
+        }
+        return json.build();
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public JsonArray add(String str) {
+        int count = 1;
+        JsonObject json = Json.createObjectBuilder()
+                .add("id", Json.createObjectBuilder()
+                        .add("author", "")
+                        .add("title", "")
+                        .add("contents", "")
+                        .add("senttime", "")
+                )
+                .build();
+        count++;
+        return getAll();
+    }
+
+//    @PUT
+//    @Produces("application/json")
+//    public JsonArray edit(String str){
+//        
+//        
+//        
+//        
+//        return json.build();
+//    }
 }
